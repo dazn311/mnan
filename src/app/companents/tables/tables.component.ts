e701efd364d1538a70c828ce796ee18e7e2e2ca1
Alexander <57184332+dazn311@users.noreply.github.com>
2020-08-15 15:27:17 +0300

    Рефакторинг таблицы на основе angular materials

--- src/app/companents/tables/tables.component.ts
+++ src/app/companents/tables/tables.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DataTablesService} from '../../sevice/data-tables.service';
 import {Task} from '../../model/Task';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-tables',
@@ -8,6 +9,11 @@ import {Task} from '../../model/Task';
   styleUrls: ['./tables.component.css']
 })
 export class TablesComponent implements OnInit {
+  // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
+  private displayedColumns: string[] = ['tast', 'text', 'folder', 'date', 'delete'];
+  private dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
+
+
   tables: Task[] = [];
   loading = false;
   LoadingMessage = 'Loading ...';
@@ -18,8 +24,9 @@ export class TablesComponent implements OnInit {
   }
 
   ngOnInit(): void {
-     this.getTables();
-     this.dataTableService.taskSubject.subscribe(task => this.tables = task);
+    this.dataSource = new MatTableDataSource();
+    this.getTables();
+    this.dataTableService.taskSubject.subscribe(task => this.tables = task);
     this.dataTableService.selectedCategorySub.subscribe(sc => this.selectedCategory = sc);
   }
 
@@ -39,11 +46,37 @@ export class TablesComponent implements OnInit {
       setTimeout(() => {
         this.loading = false;
         this.tables = this.dataTableService.getTablesByCategory('');
+        this.refreshTable();
         this.updateTables();
       }, 5000);
     }else {
       this.loading = false;
     }
   } // end updetaTables
+
+  // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
+  private refreshTable(): void {
+    this.dataSource.data = this.tables; // обновить источник данных (т.к. данные массива tasks обновились)
+  }
+
+
+  // в зависимости от статуса задачи - вернуть цвет названия
+  private getPriorityColor(task: Task) {
+
+    // цвет завершенной задачи
+    if (task.completed) {
+      return '#F8F9FA'; // TODO вынести цвета в константы (magic strings, magic numbers)
+    }
+
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+
+    return '#fff'; // TODO вынести цвета в константы (magic strings, magic numbers)
+
+  }
+
+
+
 }
 
